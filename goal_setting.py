b9c16a5a13fbc20a4f962841964f6fa229748383
Katie O Donnell <katiemodonnell88@gmail.com>
2018-10-30 14:38:50 +0000

    HTML pages added

--- goal_setting.py
+++ goal_setting.py
@@ -33,7 +33,33 @@ def add_category():
     mongo.db.create_collection(category_name)
     return redirect(url_for("get_categories"))
     
+ 
+ 
+@app.route("/goals")
+def get_goals():
+    categories = get_category_names()
+    return render_template("goals.html", categories=categories, category='Goals List')
     
+@app.route("/goals/<category>")
+def get_goals_by_category(category):
+    goals = mongo.db[category].find()
+    categories = get_category_names()
+    return render_template("goals.html", goals=goals, categories=categories, category=category)
+
+    
+@app.route("/add_goal", methods=["GET", "POST"])
+def add_goal():
+    if request.method=="POST":
+        form_values = request.form.to_dict()
+        form_values["is_done"] = "is_done" in form_values
+        category = form_values["category_name"]
+        mongo.db[category].insert_one(form_values)
+        return redirect("/goals")
+    else:
+        categories = get_category_names()
+        return render_template("add_goal.html", categories=categories)
+        
+        
 
 if __name__ == "__main__":
         app.run(host=os.getenv('IP', '0.0.0.0'), port=int(os.getenv('PORT', 8080)), debug=True)
