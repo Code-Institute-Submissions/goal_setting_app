20ed5fdba57c4c4a76a9451de734ab78496850b0
Katie O Donnell <katiemodonnell88@gmail.com>
2018-10-30 11:41:30 +0000

    return hi py file added

---
+++ goal_setting.py
@@ -0,0 +1,18 @@
+from flask import Flask
+import os
+from flask_pymongo import PyMongo
+from bson.objectid import ObjectId
+
+app = Flask(__name__)
+
+@app.route("/")
+def show_hi():
+    return "Hi"
+
+if __name__ == "__main__":
+        app.run(host=os.getenv('IP', '0.0.0.0'), port=int(os.getenv('PORT', 8080)), debug=True)
+
+
+
+
+
