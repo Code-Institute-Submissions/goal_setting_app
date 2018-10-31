806e6cb2e3d26734b8ab31076cc5f2575afe3d3a
Katie O Donnell <katiemodonnell88@gmail.com>
2018-10-31 16:31:31 +0000

    add step functionality incorporated and steps added to Mongo DB

--- goal_setting.py
+++ goal_setting.py
@@ -2,6 +2,8 @@ from flask import Flask, render_template, request, redirect, url_for
 import os
 from flask_pymongo import PyMongo
 from bson.objectid import ObjectId
+from base64 import b64encode
+import base64
 
 app = Flask(__name__)
 
@@ -49,10 +51,43 @@ def get_goals_by_category(category):
     categories = get_category_names()
     return render_template("goals.html", goals=goals, categories=categories, category=category)
 
+
+@app.route("/goals/<category>/<goal_id>")
+def goal_details(category, goal_id):
+        the_goal =  mongo.db[category].find_one({"_id": ObjectId(goal_id)})
+        return render_template('goal_detail.html', goal=the_goal)
+
+
+@app.route("/goals/<category>/<goal_id>/add_step", methods=["GET", "POST"])
+def add_step(category, goal_id):
+        the_goal =  mongo.db[category].find_one({"_id": ObjectId(goal_id)})
+        if request.method=="POST":
+        
+            step = {
+                "name": request.form["step"],
+                "is_done": False
+            }
+            
+            current_steps = the_goal.get("steps", [])
+            current_steps.append(step)
+            the_goal["steps"] = current_steps
+            
+            mongo.db[category].update({"_id":ObjectId(goal_id)},the_goal)
+        
+            return redirect(url_for("goal_details", category=category, goal_id=goal_id))
+        else:
+            categories = get_category_names()
+            return render_template("add_step.html", goal=the_goal)
+
+
+
+
+
     
 @app.route("/add_goal", methods=["GET", "POST"])
 def add_goal():
     if request.method=="POST":
+
         form_values = request.form.to_dict()
         form_values["is_done"] = "is_done" in form_values
         category = form_values["category_name"]
@@ -61,7 +96,10 @@ def add_goal():
     else:
         categories = get_category_names()
         return render_template("add_goal.html", categories=categories)
-        
+
+
+
+
         
 
 if __name__ == "__main__":
